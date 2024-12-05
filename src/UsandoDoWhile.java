55c1010351fd0054a1236b3e30d7afde0add5acf
gsmzupper <gabriel.merconi@zup.com.br>
2024-12-05 11:07:26 -0300

    Adiciona a classe UsandoDoWhile ao projeto. Esta classe java contem os codigos que exibe um vetor usando o dowhile.

---
+++ src/UsandoDoWhile.java
@@ -0,0 +1,12 @@
+public class UsandoDoWhile {
+    public static void main(String[] args) {
+        int[] vetorUneDimencional = {11, 12, 13, 14, 15};
+        int i = 0;
+
+        System.out.println("Exibe o vetor: ");
+        do {
+            System.out.println(vetorUneDimencional[i]);
+            i++;
+        } while (i < vetorUneDimencional.length);
+    }
+}
