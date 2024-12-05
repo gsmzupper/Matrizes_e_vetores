43e4ab9660779c912bc46947e6b5649208d16fc2
gsmzupper <gabriel.merconi@zup.com.br>
2024-12-05 10:35:41 -0300

    Adiciona a classe java UsandoWhile. esta classe contem um codigo java que emprime um vetor usando o while

---
+++ src/UsandoWhile.java
@@ -0,0 +1,14 @@
+public class UsandoWhile {
+    public static void main(String[] args) {
+        int[] vetorUniDimencional = {10, 20, 15, 4, 75};
+
+        int i = 0;
+
+        System.out.println("Exibe o vetor: ");
+        while (i < vetorUniDimencional.length) {
+            System.out.println(vetorUniDimencional[i]);
+            i++;
+        }
+    }
+
+}
